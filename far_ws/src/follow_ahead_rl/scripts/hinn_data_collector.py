68ae8d8c6c61c86e3aaaf8fa9af1047b2f6c3eb7
AliK604 <alik9807274@gmail.com>
2021-08-21 16:01:08 -0700

    fix HINN data Collector

--- far_ws/src/follow_ahead_rl/scripts/hinn_data_collector.py
+++ far_ws/src/follow_ahead_rl/scripts/hinn_data_collector.py
@@ -4,14 +4,15 @@ import pandas as pd
 import os
 
 from time import sleep
+from collections import deque
 
 import gym
 import gym_gazeboros_ac
 
 # Constants
 ENV_NAME = 'gazeborosAC-v0'
-N_EPISODES = 100
-STEPS_PER_EPISODE = 45
+N_EPISODES = 10
+STEPS_PER_EPISODE = 35
 ADDON_PREV_DATA = True
 
 
@@ -19,51 +20,71 @@ if __name__ == '__main__':
     env = gym.make(ENV_NAME).unwrapped
     env.set_agent(0)
 
-    save_local_1 = './model_weights/HumanIntentNetwork/list_of_human_state.csv'
-    save_local_2 = './model_weights/HumanIntentNetwork/list_of_human_state_next.csv'
+    # save_local_1 = './model_weights/HumanIntentNetwork/Saves/list_of_human_state.csv'
+    # save_local_2 = './model_weights/HumanIntentNetwork/Saves/list_of_human_state_next.csv'
+    # list_of_human_state = []
+    # list_of_human_state_next = []
 
-    list_of_human_state = []
-    list_of_human_state_next = []
+    save_local= './model_weights/HumanIntentNetwork/Saves/Human_xyTheta_ordered_triplets.csv'
+    Human_xyTheta_ordered_triplets = []
 
-    mode = 0
+    action = [0,0] 
+    mode = [0,1,2,4] # see def path_follower() is gym_gazebros.py
+    window_size = 4 # 1 is current, rest are past
+    history = deque([0]*window_size, maxlen=window_size)
+
     for i in range(N_EPISODES):
         print(f"Running episode: {i} of {N_EPISODES}")
-        env.set_person_mode(mode % 5)   # Cycle through different person modes
-        mode += 1
+        env.set_person_mode(random.choice(mode))   # Cycle through different person modes
+
         state = env.reset()
 
-        max_itr = STEPS_PER_EPISODE
-        while max_itr > 0:
-            max_itr -= 1
+        # fill with valid non-0 data
+        xyTheta = env.get_person_pos()
+        for _ in range(window_size+1):
+            history.appendleft(xyTheta)
+
+        for ii in range(STEPS_PER_EPISODE):
+
+            sleep(0.5)
+            xyTheta = env.get_person_pos()
+            history.appendleft(xyTheta)
 
             action = [0,0]
-            state, reward, done, _ = env.step(action)
+            # action = [xyTheta[0], xyTheta[1]]
+            state, reward, done, _ = env.step(action) # i doubt this even matters... 
 
-            human_state = list(state)
-            list_of_human_state.append(human_state) # current x,y,theta AND WITH it's history    to next x,y, theta 
+            # print(f'history as list is\n{list(history)}') # we are saving a moving window. it's not efficient, but that shouldnt matter
+            Human_xyTheta_ordered_triplets.append(list(history))
 
-            sleep(0.1)
-
-            state, reward, done, _ = env.step(action)
-
-            xy = env.get_person_pos()
-            next_state = [xy[0], xy[1], state[2]]
-            list_of_human_state_next.append(next_state)
+            ''' past code
+                state, reward, done, _ = env.step(action)
+                human_state = list(state)
+                list_of_human_state.append(human_state) # current x,y,theta AND WITH it's history    to next x,y, theta 
+                sleep(0.1)
+                xy = env.get_person_pos()
+                next_state = [xy[0], xy[1], xy[2]]
+                list_of_human_state_next.append(next_state)
+            '''
+            # if env.fallen:
+            #     break
 
     env.close()
 
     if ADDON_PREV_DATA:
-        if os.path.isfile(save_local_1) and os.path.isfile(save_local_2):
-            list_of_human_state.extend(pd.read_csv(save_local_1).values.tolist())
-            list_of_human_state_next.extend(pd.read_csv(save_local_2).values.tolist())
+        if os.path.isfile(save_local):
+            Human_xyTheta_ordered_triplets.extend(pd.read_csv(save_local).values.tolist())
         else:
             print("Warning: Tried to load previous data but files were not found!")
 
-
     # save data
-    _ = pd.DataFrame(list_of_human_state).to_csv(
-        save_local_1, header=False, index=False)
-    _ = pd.DataFrame(list_of_human_state_next).to_csv(
-        save_local_2, header=False, index=False)
+    _ = pd.DataFrame(Human_xyTheta_ordered_triplets).to_csv(save_local, header=False, index=False)
+    print("Done Saving...\nEND")
 
-    print("END")
+    '''
+    if os.path.isfile(save_local_1) and os.path.isfile(save_local_2):
+        list_of_human_state.extend(pd.read_csv(save_local_1).values.tolist())
+        list_of_human_state_next.extend(pd.read_csv(save_local_2).values.tolist())
+        _ = pd.DataFrame(list_of_human_state).to_csv(save_local_1, header=False, index=False)
+        _ = pd.DataFrame(list_of_human_state_next).to_csv(save_local_2, header=False, index=False)
+    '''
